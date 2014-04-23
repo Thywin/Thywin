aa99ee69986f8671a48d93a20b07bebb791467ef
Thomas Kooi <thomasskooi@live.nl>
2014-04-23 11:39:43 +0200

    added main.cpp files

--- source/scheduler/main.cpp
+++ source/scheduler/main.cpp
@@ -1,10 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 23 apr. 2014
+ *      Author: Thomas
+ */
+
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char** argv)
 {
-	printf("Hello Crawler!\n");
+	printf("Hello Scheduler!");
 
 	return EXIT_SUCCESS;
 }
+
+
