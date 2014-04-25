da6c10e4046035b0672a6023ec8439cb323b94e5
Thomas Kooi <thomasskooi@live.nl>
2014-04-25 13:52:00 +0200

    Crawler cpp files added

---
+++ source/crawler/crawler.cpp
@@ -0,0 +1,8 @@
+/*
+ * crawler.cpp
+ *
+ *  Created on: 25 apr. 2014
+ *      Author: Thomas
+ */
+
+#include "crawler.hpp"
---
+++ source/crawler/crawler.hpp
@@ -0,0 +1,52 @@
+/*
+ * crawler.hpp
+ *
+ *  Created on: 25 apr. 2014
+ *      Author: Thomas
+ */
+
+#ifndef CRAWLER_HPP_
+#define CRAWLER_HPP_
+#define MAX_CRAWL_LIST 10
+
+namespace crawler
+{
+	class URL
+	{
+		public:
+			char* getURL();
+		private:
+			char* url;
+	};
+	class Parser
+	{
+		public:
+			char *getIP();
+		private:
+			char *ip;
+	};
+
+	class Scheduler
+	{
+		public:
+			char *getIP();
+		private:
+			char *ip;
+	};
+	class Crawler
+	{
+	public:
+
+	protected:
+
+
+	private:
+		URL crawlList[MAX_CRAWL_LIST];
+		void findParser();
+		void findScheduler();
+		void requestNextURL();
+		void crawlNext();
+	};
+
+}
+#endif /* CRAWLER_HPP_ */
