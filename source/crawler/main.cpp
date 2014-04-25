e54e20ab9c1b737197c043db0c79d514a0320099
Thomas Kooi <thomasskooi@live.nl>
2014-04-25 15:40:25 +0200

    Prototype crawler implemented

--- source/crawler/main.cpp
+++ source/crawler/main.cpp
@@ -2,9 +2,12 @@
 #include <cstdio>
 #include <cstring>
 #include <stdlib.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+#include <fcntl.h>
 
 int count = 0;
-
+int fileID = -1;
 void OnBegin(const happyhttp::Response* r, void* userdata)
 {
 	printf("BEGIN (%d %s)\n", r->getstatus(), r->getreason());
@@ -15,15 +18,22 @@ void OnData(const happyhttp::Response* r, void* userdata,
 		const unsigned char* data, int n)
 {
 	// fwrite(data, 1, n, stdout);
+
+	if (write(fileID, data,n) < 1) {
+		perror("failed to write");
+	} else {
+		//printf("%c",data);
+		fwrite(data, 1, n, stdout);
+	}
 	count += n;
 }
 
 void OnComplete(const happyhttp::Response* r, void* userdata)
 {
-	printf("COMPLETE (%d bytes)\n", count);
+	printf("COMPLETE (%d bytes)\n\n", count);
 }
 
-void testRequest(char* site, char* site2)
+void sendRequestTest(char* site, char* site2)
 {
 	happyhttp::Connection conn(site, 80);
 	conn.setcallbacks(OnBegin, OnData, OnComplete, 0);
@@ -38,10 +48,21 @@ void testRequest(char* site, char* site2)
 
 int main(int argc, char** argv)
 {
-
+	char *collection[3] = {"www.google.nl","kooisoftware.nl","www.nu.nl"};
 	try
 	{
-		testRequest(argv[1], argv[2]);
+		for (int i = 0; i < 3; i++) {
+			fileID = open(collection[i],O_CREAT | O_WRONLY, 0777);
+			if (fileID < 0) {
+				perror("failed to create file");
+			} else {
+				printf("Opening: %s",collection[i]);
+				sendRequestTest(collection[i],"/");
+				printf("Closing file\n");
+				close(fileID);
+			}
+		}
+		//sendRequestTest(argv[1], argv[2]);
 	}
 
 	catch (happyhttp::Wobbly& e)
