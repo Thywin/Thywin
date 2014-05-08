e1d53319deab275e81afec008856e081252d9d55
Erwin Janssen <erwinjanssen@outlook.com>
2014-05-08 15:13:22 +0200

    DetermineRelevance function added

--- source/parser/RelevanceDeterminator.h
+++ source/parser/RelevanceDeterminator.h
@@ -8,10 +8,14 @@
 #ifndef RELEVANCEDETERMINATOR_H_
 #define RELEVANCEDETERMINATOR_H_
 
+#include <map>
+#include <string>
+
 class RelevanceDeterminator {
 public:
 	RelevanceDeterminator();
 	virtual ~RelevanceDeterminator();
+	double DetermineRelevance(std::map<std::string, unsigned int> firstDocumentVector, std::map<std::string, unsigned int> secondDocumentVector);
 };
 
 #endif /* RELEVANCEDETERMINATOR_H_ */
