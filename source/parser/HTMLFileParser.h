d2db6c1c6d3dcdeae8103310e084e32a812110d2
Thomas Gerritsen <thomas.gerritsen@live.nl>
2014-05-12 14:20:37 +0200

    Added commentary for the URI extractor
    Doxygen (Javadoc) comments added to the HTMLFileParser class.

--- source/parser/HTMLFileParser.h
+++ source/parser/HTMLFileParser.h
@@ -21,7 +21,21 @@ namespace thywin
 			HTMLFileParser();
 			virtual ~HTMLFileParser();
 
+			/**
+			 * Returns a list of URIs extracted from the contents of a file.
+			 * The content argument must contain the code of a page in it's entirety.
+			 * The source argument contains the URI of the page where the content came from.
+			 * The returned URI are constructed according to the source that is given with the content.
+			 *
+			 * @param	content	a string containing the code of a page
+			 * @param	source	the URI of the page
+			 * @return			the list of the URI found in the content
+			 */
 			std::vector<std::string> ExtractURIs(std::string content, std::string source);
+
+			/**
+			 *
+			 */
 			std::string ExtractText(std::string content);
 		private:
 			std::vector<std::string> parseString(std::string input, std::string::size_type endHeadPos,
