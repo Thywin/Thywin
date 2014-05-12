46bcd17060d681f5609a43cb13e2d640eb8b005c
Erwin Janssen <erwinjanssen@outlook.com>
2014-05-12 11:20:02 +0200

    Changed: Refactored the DocumentVector class based on code review feedback.
    - General formatting
    - Improved documentation
    - In CalculateSimilarity: now checking for a magnitude of zero.
    - Changed the map<string, unsigned int> to DocumentVector.
    - Removed test document

--- source/parser/DocumentVector.cpp
+++ source/parser/DocumentVector.cpp
@@ -10,100 +10,97 @@
 #include <algorithm>
 #include <iostream>
 
-namespace thywin {
-
-DocumentVector::DocumentVector()
+namespace thywin
 {
-}
-DocumentVector::DocumentVector(std::string text)
-{
-	if(text.size() == 0)
+	
+	DocumentVector::DocumentVector()
 	{
-		return;
 	}
-	std::transform(text.begin(), text.end(), text.begin(), ::tolower);
-
-	bool extractingWord = true;
-	if(text.at(0) < 'a' || text.at(0) > 'z')
+	DocumentVector::DocumentVector(std::string text)
 	{
-		extractingWord = false;
-	}
-	while(!text.empty())
-	{
-		for(unsigned int i = 0; i < text.size(); i++)
+		if (text.size() == 0)
 		{
-			if(extractingWord && (text.at(i) < 'a' || text.at(i) > 'z'))
+			return;
+		}
+		std::transform(text.begin(), text.end(), text.begin(), ::tolower);
+		
+		bool extractingWord = true;
+		if (text.at(0) < 'a' || text.at(0) > 'z')
+		{
+			extractingWord = false;
+		}
+		
+		unsigned int lastIndex = 0;
+		for (unsigned int i = 0; i < text.size(); i++)
+		{
+			if (extractingWord && (text.at(i) < 'a' || text.at(i) > 'z'))
 			{
-				(*this)[text.substr(0, i)]++;
-				text.erase(0, i);
+				(*this)[text.substr(lastIndex, i - lastIndex)]++;
+				lastIndex = i;
 				extractingWord = false;
-				break;
 			}
-			else if(!extractingWord && !(text.at(i) < 'a' || text.at(i) > 'z'))
+			else if (!extractingWord && !(text.at(i) < 'a' || text.at(i) > 'z'))
 			{
-				text.erase(0, i);
+				lastIndex = i;
 				extractingWord = true;
-				break;
 			}
-
-			if(i == text.size() - 1)
+			else if ((i == text.size() - 1) && extractingWord)
 			{
-				if(extractingWord)
-				{
-					(*this)[text.substr(0, i + 1)]++;
-				}
-				text.clear();
-				break;
+				(*this)[text.substr(lastIndex, i - lastIndex + 1)]++;
+			}
+			// else omitted because there are no other cases that will be handled.
+		}
+	}
+	
+	DocumentVector::~DocumentVector()
+	{
+	}
+	
+	double DocumentVector::CalculateSimilarity(DocumentVector* documentVector)
+	{
+		double magnitude = this->GetMagnitude() * documentVector->GetMagnitude();
+		if (magnitude == 0)
+		{
+			return 0;
+		}
+		return this->DotProduct(documentVector) / magnitude;
+	}
+	
+	double DocumentVector::GetMagnitude()
+	{
+		double magnitude = 0;
+		for (DocumentVector::iterator i = this->begin(); i != this->end(); i++)
+		{
+			magnitude += std::pow(i->second, 2);
+		}
+		return std::sqrt(magnitude);
+	}
+	
+	double DocumentVector::DotProduct(DocumentVector* documentVector)
+	{
+		double dotProduct = 0;
+		DocumentVector* largestDocumentVector = NULL;
+		DocumentVector* smallestDocumentVector = NULL;
+		if (this->size() > documentVector->size())
+		{
+			largestDocumentVector = this;
+			smallestDocumentVector = documentVector;
+		}
+		else
+		{
+			largestDocumentVector = documentVector;
+			smallestDocumentVector = this;
+		}
+		
+		for (DocumentVector::iterator i = smallestDocumentVector->begin(); i != smallestDocumentVector->end(); i++)
+		{
+			DocumentVector::iterator searchResult = largestDocumentVector->find(i->first);
+			if (searchResult != largestDocumentVector->end())
+			{
+				dotProduct += i->second * searchResult->second;
 			}
 		}
+		return dotProduct;
 	}
-}
-
-DocumentVector::~DocumentVector()
-{
-}
-
-double DocumentVector::CalculateSimilarity(DocumentVector* documentVector)
-{
-	return this->DotProduct(documentVector) / (this->GetMagnitude() * documentVector->GetMagnitude());
-}
-
-double DocumentVector::GetMagnitude()
-{
-	double magnitude = 0;
-	for(std::map<std::string, unsigned int>::iterator i = this->begin(); i != this->end(); i++)
-	{
-		magnitude += std::pow(i->second, 2);
-	}
-	return std::sqrt(magnitude);
-}
-
-double DocumentVector::DotProduct(DocumentVector* documentVector)
-{
-	double dotProduct = 0;
-	DocumentVector* largestDocumentVector = NULL;
-	DocumentVector* smallestDocumentVector = NULL;
-	if(this->size() > documentVector->size())
-	{
-		largestDocumentVector = this;
-		smallestDocumentVector = documentVector;
-	}
-	else
-	{
-		largestDocumentVector = documentVector;
-		smallestDocumentVector = this;
-	}
-
-	for(std::map<std::string, unsigned int>::iterator i = smallestDocumentVector->begin(); i != smallestDocumentVector->end(); i++)
-	{
-		std::map<std::string, unsigned int>::iterator searchResult = largestDocumentVector->find(i->first);
-		if(searchResult != largestDocumentVector->end())
-		{
-			dotProduct += i->second * searchResult->second;
-		}
-	}
-	return dotProduct;
-}
-
 
 } /* namespace thywin */
--- source/parser/DocumentVector.h
+++ source/parser/DocumentVector.h
@@ -11,37 +11,52 @@
 #include <map>
 #include <string>
 
-namespace thywin {
+namespace thywin
+{
+	
+	class DocumentVector: public std::map<std::string, unsigned int>
+	{
+		public:
+			
+			/**
+			 * Default constructor
+			 * Use this to create an empty DocumentVector and manually fill it later.
+			 */
+			DocumentVector();
 
-class DocumentVector: public std::map<std::string, unsigned int> {
-public:
-	DocumentVector();
+			/**
+			 * This constructor takes a string as argument.
+			 * Creates a DocumentVector based on the input string. Sets the text to lower case,
+			 * then counts the words and creates a DocumentVector based on the number of occurrences of every word.
+			 * @param text The input text of which a DocumentVector must be created.
+			 */
+			DocumentVector(std::string text);
 
-	/**
-	 * This constructor takes a string as argument.
-	 * Converts the input string to lowercase, counts the words and creates a DocumentVector
-	 */
-	DocumentVector(std::string text);
+			/**
+			 * Default destructor
+			 */
+			virtual ~DocumentVector();
 
-	virtual ~DocumentVector();
+			/**
+			 * Computes the cosine similarity between the DocumentVector it is called upon
+			 * and the DocumentVector passed as argument.
+			 * @param documentVector The DocumentVector that will be compared.
+			 * @return The similarity between the two DocumentVectors, a double with a value between 0 and 1 (both inclusive)
+			 */
+			double CalculateSimilarity(DocumentVector* documentVector);
 
-	/**
-	 * Computes the cosine similairy between the DocumentVector it is called upon
-	 * and the DocumentVector passed as argument.
-	 */
-	double CalculateSimilarity(DocumentVector* documentVector);
+			/**
+			 * Computes the magnitude (length) of this DocumentVector.
+			 * @return The magnitude (length) of this DocumentVector, this value will be 0 or greater.
+			 */
+			double GetMagnitude();
 
-	/**
-	 * Computes the magnitude (length) of the DocumentVector
-	 */
-	double GetMagnitude();
-
-	/**
-	 * Computes the dot product between the DocumentVector it is called upon
-	 * and the DocumentVector passed as argument.
-	 */
-	double DotProduct(DocumentVector* documentVector);
-};
+			/**
+			 * Computes the dot product between the DocumentVector it is called upon and the DocumentVector passed as argument.
+			 * @return The dot product between the two DocumentVectors, this value will be 0 or greater.
+			 */
+			double DotProduct(DocumentVector* documentVector);
+	};
 
 } /* namespace thywin */
 
