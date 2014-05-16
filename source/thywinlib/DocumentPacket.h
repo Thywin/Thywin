3cf44e4a95a288d32edc72bdbae8d45f158dc366
Thomas Kooi <thomasskooi@live.nl>
2014-05-16 11:04:47 +0200

    Added documentation in header files
    Added documentation for Communicator methods and const variables.
    Added documentation for DocumentPacket
    Added documentation for ThywinPacket

--- source/thywinlib/DocumentPacket.h
+++ source/thywinlib/DocumentPacket.h
@@ -12,15 +12,35 @@
 namespace thywin
 {
 
+	/**
+	 * Object to store documents and their location on the web.
+	 * Has build in functionality to serialize and deserialize for
+	 * sending across the network between clients and server.
+	 */
 	class DocumentPacket: public TPObject
 	{
 		public:
-			std::string Serialize();
-			void Deserialize(std::string);
-			int URISize;
+			/**
+			 * String containing a URI, location of the document on the web.
+			 */
 			std::string URI;
-			int DocumentSize;
+
+			/**
+			 * Contains a document. Usually the entire content of a web page.
+			 */
 			std::string Document;
+
+			/**
+			 * Serializes the DocumentPacket to a string so it can be send over a socket
+			 * @return	String version of the DocumentPacket.
+			 */
+			std::string Serialize();
+
+			/**
+			 * Deserializes a string to a DocumentPacket. Fills the URI and Document with the content from the string.
+			 *
+			 */
+			void Deserialize(std::string);
 	};
 
 } /* namespace thywin */
--- source/thywinlib/ThywinPacket.h
+++ source/thywinlib/ThywinPacket.h
@@ -15,20 +15,53 @@
 
 namespace thywin
 {
-	enum PacketType
-	{
-		URI = 1, DOCUMENT = 2, RELEVANCE = 3, DOCUMENTVECTOR = 4
-	};
-
+	/**
+	 * @GET			Request content of the given type @PacketType
+	 * @PUT			Send a put message of the given type @PacketType
+	 * @RESPONSE	Reply to a GET request. Expected to be of same @PacketType as the GET request
+	 *
+	 * Defines the type of Method from a message between client and server.
+	 * Can be any of the above listed. RESPONSE is a reply on a GET message.
+	 */
 	enum PacketMethod
 	{
 		GET = 1, PUT = 2, RESPONSE = 3
 	};
 
+	/**
+	 * @URI				Uniform resource identifier
+	 * @DOCUMENT		A Document or content of a web page
+	 * @RELEVANCE
+	 * @DOCUMENTVECTOR
+	 *
+	 * PacketType is the type of a ThywinPacket when send across the network between client and server.
+	 * The type identifies the content send within the package.
+	 */
+	enum PacketType
+	{
+		URI = 1, DOCUMENT = 2, RELEVANCE = 3, DOCUMENTVECTOR = 4
+	};
+
+
+	/**
+	 * Struct for sending over messages between clients and servers.
+	 */
 	struct ThywinPacket
 	{
+			/**
+			 * Contains the method of the ThywinPacket. @PacketMethod
+			 */
 			PacketMethod Method;
+
+			/**
+			 * Contains the type of the ThywinPacket. @PacketType
+			 */
 			PacketType Type;
+
+			/**
+			 * Shared pointer to the content of the packet. This is the message/information that will be send across.
+			 * When using the @PacketMethod GET, Content can be of value NULL.
+			 */
 			std::shared_ptr<TPObject> Content;
 	};
 
