df683a4c3b7dc1ee69cc35f88e1197fdee360c3d
Imre Woudstra <imre.woudstra@gmail.com>
2014-05-16 10:58:22 +0200

    Added Documentation in header files
    Added documentation for TPObject methods
    Added documentation for URIPacket methods

--- source/thywinlib/URIPacket.h
+++ source/thywinlib/URIPacket.h
@@ -11,15 +11,26 @@
 
 namespace thywin
 {
-
+	/**
+	 * An URIPacket is a packet which contains a URI and the Relevance of that URI.
+	 */
 	class URIPacket: public TPObject
 	{
 		public:
 			double Relevance;
 			std::string URI;
 
+			/**
+			 * Serialize the URIPacket to a string so it can be sent over a socket.
+			 * @return the string that represents the serialized URIPacket
+			 */
 			std::string Serialize();
-			void Deserialize(std::string);
+
+			/**
+			 * Deserialize a string to an URIPacket
+			 * @param input serialized URIPacket that will be deserialized to a URIPacket
+			 */
+			void Deserialize(std::string input);
 	};
 } /* namespace thywin */
 
