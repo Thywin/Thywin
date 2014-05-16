0d1095c1e2d55eb00dead2af9991fa6a305e695c
Imre Woudstra <imre.woudstra@gmail.com>
2014-05-16 10:58:22 +0200

    Added Documentation in header files
    Added documentation for TPObject methods
    Added documentation for URIPacket methods

--- source/thywinlib/TPObject.h
+++ source/thywinlib/TPObject.h
@@ -12,15 +12,34 @@
 
 namespace thywin
 {
+	/**
+	 * A class that implements TPObject can be sent over a socket via the Serialize and Deserialize methods
+	 */
 	class TPObject
 	{
 		public:
+			/**
+			 * Virtual method to serialize a TPObject which will be sent over a socket.
+			 * @return a string that represents the serialized object.
+			 */
 			virtual std::string Serialize() = 0;
-			virtual void Deserialize(std::string) = 0;
 
+			/**
+			 * Virtual method to deserialize a String to a TPObject which was sent over a socket.
+			 * @input the string that represents the serialized object
+			 */
+			virtual void Deserialize(std::string input) = 0;
+
+			/**
+			 * Empty constructor
+			 */
 			TPObject()
 			{
 			}
+
+			/**
+			 * Empty destructor
+			 */
 			virtual ~TPObject()
 			{
 			}
