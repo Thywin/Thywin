414b764aed39d24b7fd494ca6dad87309b5bc86a
Thomas Kooi <thomasskooi@live.nl>
2014-05-20 10:34:29 +0200

    Now expects URIPacket & Document Packet from DB

--- source/master/Master.cpp
+++ source/master/Master.cpp
@@ -18,8 +18,6 @@
 #include "DatabaseHandler.h"
 #include <algorithm>
 
-
-
 namespace thywin
 {
 //	std::vector<std::shared_ptr<URIPacket>> Master::URIQueue;
@@ -30,10 +28,12 @@ namespace thywin
 	sem_t Master::documentQueueNotEmpty;
 	DatabaseHandler Master::DBConnection("192.168.100.13", 5432);
 
-	void Master::InitializeMaster() {
-		sem_init(&documentQueueNotEmpty,0,0);
+	void Master::InitializeMaster()
+	{
+		sem_init(&documentQueueNotEmpty, 0, 0);
 		DBConnection.Connect();
 	}
+
 	void Master::AddURIElementToQueue(std::shared_ptr<URIPacket> element)
 	{
 		Master::URIQueueMutex.lock();
@@ -50,36 +50,16 @@ namespace thywin
 			Master::fillURLQueue();
 			/* Temporary queue filling for debug purposes. */
 		}
-		std::shared_ptr<URIPacket> element(new URIPacket);
-		std::vector<std::string> uri = DBConnection.RetrieveAndDeleteURIFromQueue();
-		element->URI = uri.at(0);
-		element->Relevance = atof(uri.at(1).c_str());
+		std::shared_ptr<URIPacket> element = DBConnection.RetrieveAndDeleteURIFromQueue();
 		Master::URIQueueMutex.unlock();
 		return element;
-	}
-	bool replace(std::string& str, const std::string& from, const std::string& to) {
-	    size_t start_pos = str.find(from);
-	    if(start_pos == std::string::npos)
-	        return false;
-	    str.replace(start_pos, from.length(), to);
-	    return true;
 	}
 
 	void Master::AddDocumentElementToQueue(std::shared_ptr<DocumentPacket> element)
 	{
 		Master::DocumentQueueMutex.lock();
 		printf("AddDocumentElementToQueue\n");
-
-
-		printf("Replacing stuff\n");
-
-		std::stringstream keybuffer;
-		keybuffer << (char)3;
-		while (replace(element->Document, "'", keybuffer.str())) {};
-		while (replace(element->Document,keybuffer.str(), "''")) {};
-		printf("Replaced stuff\n");
-		//element->Document.replace("'","''");
-		DBConnection.AddDocumentToQueue(element->URI,element->Document);
+		DBConnection.AddDocumentToQueue(element);
 		sem_post(&documentQueueNotEmpty);
 		Master::DocumentQueueMutex.unlock();
 	}
@@ -88,11 +68,7 @@ namespace thywin
 	{
 		sem_wait(&documentQueueNotEmpty);
 		Master::DocumentQueueMutex.lock();
-			std::shared_ptr<DocumentPacket> element(new DocumentPacket);
-			std::vector<std::string> doc = DBConnection.RetrieveAndDeleteDocumentFromQueue();
-			std::shared_ptr<DocumentPacket> returnDoc(new DocumentPacket);
-			element->URI = doc.at(0);
-			element->Document = doc.at(1);
+		std::shared_ptr<DocumentPacket> element = DBConnection.RetrieveAndDeleteDocumentFromQueue();
 		Master::DocumentQueueMutex.unlock();
 		return element;
 	}
@@ -103,9 +79,9 @@ namespace thywin
 		URIElement->URI =
 				"http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=685270&url=http%3A%2F%2Fieeexplore.ieee.org%2Fiel4%2F5611%2F15013%2F00685270.pdf%3Farnumber%3D685270\0";
 		URIElement->Relevance = 0.5;
-
 		DBConnection.AddURIToList(URIElement);
 		DBConnection.AddURIToQueue(URIElement->URI);
+
 		std::shared_ptr<URIPacket> otherElement(new URIPacket);
 		otherElement->URI = "http://en.wikipedia.org/wiki/Discrete_event_simulation\0";
 		otherElement->Relevance = 0.01;
@@ -115,7 +91,6 @@ namespace thywin
 		std::shared_ptr<URIPacket> anotherElement(new URIPacket);
 		anotherElement->URI = "http://www.reliasoft.com/reno/features1.htm\0";
 		anotherElement->Relevance = 0.01;
-
 		DBConnection.AddURIToList(anotherElement);
 		DBConnection.AddURIToQueue(anotherElement->URI);
 	}
