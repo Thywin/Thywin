327fdb4595971cc4c3ece9c9b8db6a71cc0409cf
Bobby Bouwmann <bobbybouwmann@gmail.com>
2014-05-26 15:46:51 +0200

    Pull request feedback: Last documentation added

--- source/thywinlib/MultiURIPacket.h
+++ source/thywinlib/MultiURIPacket.h
@@ -17,25 +17,25 @@
 namespace thywin
 {
 	/**
-	 *
+	 * A MultiURIPacket is a packet which contains multiple @URIPacket (s)
 	 */
 	class MultiURIPacket: public ThywinPacketContent
 	{
 		public:
 			/**
-			 *
+			 * Content represents a vector of multiple URIs
 			 */
 			std::vector<std::shared_ptr<URIPacket>> Content;
 
 			/**
 			 * Serializes the DocumentPacket to a string so it can be send over a socket
-			 * @return	String version of the DocumentPacket.
+			 * @return String version of the DocumentPacket.
 			 */
 			std::string Serialize();
 
 			/**
 			 * Deserializes a string to a MultiURIPacket. Fills the URI and Document with the content from the string.
-			 *
+			 * @param input serialized MultiURIPacket that will be deserialized to a MultiURIPacket
 			 */
 			void Deserialize(const std::string& input);
 	};
